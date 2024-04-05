9513b3989ae1ad8b9d7c07317ddc14ade5cbff7a
Fatali Fataliyev <fataliyevfatali2001@gmail.com>
2024-04-05 13:28:00 -0400

    First Commit

---
+++ F1_Manager.Models/User/UserDetail.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace F1_Manager.Models.User
+{
+    internal class UserDetail
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Role { get; set; }
+    }
+}
---
+++ HAAS_F1_Manager_Sim/Areas/Identity/Pages/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HAAS_F1_Manager_Sim.Areas.Identity.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
